29d678fb752388c887f7cec840b48ec3ecc3a025
David Cai <43160344+davidxcai@users.noreply.github.com>
2025-05-18 15:34:10 -0500

    authorization updated & profile RLS restored

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -8,38 +8,40 @@ import type { Session } from "@supabase/supabase-js";
 const AuthContext = createContext<Session | null | undefined>(undefined);
 
 type AuthProviderProps = {
-  children: ReactNode;
+    children: ReactNode;
 };
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  console.count("AuthProvider");
-  const queryClient = useQueryClient();
-  const session = useGetSession();
+    console.count("AuthProvider");
+    const queryClient = useQueryClient();
+    const session = useGetSession();
 
-  useEffect(() => {
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange(async (_event, session) => {
-      queryClient.setQueryData(["session"], session);
-    });
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, [queryClient]);
+    useEffect(() => {
+        const {
+            data: { subscription },
+        } = supabase.auth.onAuthStateChange(async (_event, session) => {
+            queryClient.setQueryData(["session"], session);
+        });
+        return () => {
+            subscription.unsubscribe();
+        };
+    }, [queryClient]);
 
-  if (session.isPending) return <div>Loading session...</div>;
+    if (session.isPending) return <div>Loading session...</div>;
 
-  if (session.isError) return <div>Error: {session.error.message}</div>;
+    if (session.isError) return <div>Error: {session.error.message}</div>;
 
-  return (
-    <AuthContext.Provider value={session.data}>{children}</AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={session.data}>
+            {children}
+        </AuthContext.Provider>
+    );
 }
 
 export const useAuthContext = () => {
-  const context = useContext(AuthContext);
-  if (context === undefined) {
-    throw new Error("useAuthContext must be used within an AuthProvider");
-  }
-  return context;
+    const context = useContext(AuthContext);
+    if (context === undefined) {
+        throw new Error("useAuthContext must be used within an AuthProvider");
+    }
+    return context;
 };
